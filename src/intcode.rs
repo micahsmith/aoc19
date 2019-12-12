d8221a32139f9e6720ed0e052392e6d1537b2e6e
Micah Smith <micahadamsmith@gmail.com>
2019-12-11 21:42:34 -0500

    added new instructions

--- src/intcode.rs
+++ src/intcode.rs
@@ -31,11 +31,18 @@ impl IntCodeProgram {
 
         loop {
             let (opcode, p_one, p_two, p_three) = self.get_opcode_and_parameters(idx);
+            println!("{:?}", self.program);
+            println!("{:?}", idx);
+            println!("{:?}", (opcode, p_one, p_two, p_three));
             match opcode {
                 1 => self.opcode_one(&mut idx, p_one, p_two, p_three),
                 2 => self.opcode_two(&mut idx, p_one, p_two, p_three),
                 3 => self.opcode_three(&mut idx, p_one),
                 4 => self.opcode_four(&mut idx, p_one),
+                5 => self.opcode_five(&mut idx, p_one, p_two),
+                6 => self.opcode_six(&mut idx, p_one, p_two),
+                7 => self.opcode_seven(&mut idx, p_one, p_two, p_three),
+                8 => self.opcode_eight(&mut idx, p_one, p_two, p_three),
                 99 => break,
                 _ => panic!("Unknown opcode: {}", opcode),
             }
@@ -63,6 +70,40 @@ impl IntCodeProgram {
     fn opcode_four(&mut self, idx: &mut usize, one: usize) {
         println!("output: {}", self.program[one]);
         *idx += 2;
+    }
+
+    fn opcode_five(&mut self, idx: &mut usize, one: usize, two: usize) {
+        if self.program[one] != 0 {
+            *idx = self.program[two] as usize;
+        } else {
+            *idx += 3;
+        }
+    }
+
+    fn opcode_six(&mut self, idx: &mut usize, one: usize, two: usize) {
+        if self.program[one] == 0 {
+            *idx = self.program[two] as usize;
+        } else {
+            *idx += 3;
+        }
+    }
+
+    fn opcode_seven(&mut self, idx: &mut usize, one: usize, two: usize, three: usize) {
+        if self.program[one] > self.program[two] {
+            self.program[three] = 1;
+        } else {
+            self.program[three] = 0;
+        }
+        *idx += 4;
+    }
+
+    fn opcode_eight(&mut self, idx: &mut usize, one: usize, two: usize, three: usize) {
+        if self.program[one] == self.program[two] {
+            self.program[three] = 1;
+        } else {
+            self.program[three] = 0;
+        }
+        *idx += 4;
     }
 
     fn get_opcode_and_parameters(&self, idx: usize) -> (i32, usize, usize, usize) {
