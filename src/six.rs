6f7f8af24b634f0bb7bc8038e03d289428a4f8ae
Micah Smith <micahadamsmith@gmail.com>
2019-12-13 21:03:26 -0500

    day six complete

--- src/six.rs
+++ src/six.rs
@@ -1,53 +1,59 @@
 use std::collections::HashMap;
-use std::sync::Arc;
 
 pub fn start(input: &str) {
-    let mut map: HashMap<&str, Arc<OrbitNode>> = HashMap::new();
+    let mut map: HashMap<&str, &str> = HashMap::new();
 
     input
         .trim()
         .split("\n")
         .map(|orbit| parse_orbit(orbit))
         .for_each(|(orbitted, orbitter)| {
-            let mut node = OrbitNode::new(orbitter);
-            if let Some(p) = map.get(orbitted) {
-                node.clone().add_parent(p);
-            }
-
-            map.insert(orbitter, node);
-            
-
-
-
+            map.insert(orbitter, orbitted);
         });
-}
 
-#[derive(Clone, Debug)]
-struct OrbitNode {
-    name: String,
-    children: Vec<Arc<OrbitNode>>,
-    parent: Option<Arc<OrbitNode>>,
-}
-
-impl OrbitNode {
-    pub fn new(name: &str) -> Arc<Self> {
-        return Arc::new(OrbitNode {
-            name: String::from(name),
-            children: Vec::new(),
-            parent: None,
-        });
-    }
-
-    pub fn add_child(&mut self, child: &Arc<Self>) {
-        self.children.push(child.clone());
-    }
-
-    pub fn add_parent(&mut self, parent: &Arc<Self>) {
-        self.parent = Some(parent.clone());
-    }
+    find_number_of_orbits(&map);
+    find_shortest_path(&map);
 }
 
 fn parse_orbit(orbit: &str) -> (&str, &str) {
     let mut parsed = orbit.split(")");
     return (parsed.next().unwrap(), parsed.next().unwrap());
+}
+
+fn find_number_of_orbits(map: &HashMap<&str, &str>) {
+    let mut counter = 0;
+    for k in map.keys() {
+        let mut parent = map.get(k).unwrap();
+        counter += 1;
+        while *parent != "COM" {
+            parent = map.get(parent).unwrap();
+            counter += 1;
+        }
+    }
+
+    println!("Number of direct and indirect orbits: {}", counter);
+}
+
+fn find_shortest_path(map: &HashMap<&str, &str>) {
+    let mut santa_path = path_to_com("SAN", map);
+    let mut you_path = path_to_com("YOU", map);
+
+    while santa_path[0] == you_path[0] {
+        santa_path.remove(0);
+        you_path.remove(0);
+    }
+
+    println!("Transfers required: {}", santa_path.len() + you_path.len());
+}
+
+fn path_to_com<'a>(name: &'a str, map: &HashMap<&'a str, &'a str>) -> Vec<&'a str> {
+    let mut path: Vec<&str> = Vec::new();
+    let mut parent = map.get(name).unwrap();
+    while *parent != "COM" {
+        path.push(parent);
+        parent = map.get(parent).unwrap();
+    }
+    path.push(parent);
+    path.reverse();
+    return path;
 }
