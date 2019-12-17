28ddaf1f281e2569e0ff6ee5c376c0eca52757ba
Micah Smith <micahadamsmith@gmail.com>
2019-12-16 21:08:16 -0500

    housekeeping: added input and src files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use std::{env, fs};
 
 mod eight;
+mod eighteen;
+mod eleven;
+mod fifteen;
 mod five;
 mod four;
+mod fourteen;
 mod intcode;
 mod nine;
+mod nineteen;
 mod one;
 mod seven;
+mod seventeen;
 mod six;
+mod sixteen;
 mod ten;
+mod thirteen;
 mod three;
+mod twelve;
+mod twenty;
+mod twentyfive;
+mod twentyfour;
+mod twentyone;
+mod twentythree;
+mod twentytwo;
 mod two;
 
 fn get_input(filename: &str) -> String {
@@ -29,6 +44,21 @@ fn call_from_str(s: &str) {
         "eight" => eight::start(&get_input("inputs/eight")),
         "nine" => nine::start(&get_input("inputs/nine")),
         "ten" => ten::start(&get_input("inputs/ten")),
+        "eleven" => eleven::start(&get_input("inputs/eleven")),
+        "twelve" => twelve::start(&get_input("inputs/twelve")),
+        "thirteen" => thirteen::start(&get_input("inputs/thirteen")),
+        "fourteen" => fourteen::start(&get_input("inputs/fourteen")),
+        "fifteen" => fifteen::start(&get_input("inputs/fifteen")),
+        "sixteen" => sixteen::start(&get_input("inputs/sixteen")),
+        "seventeen" => seventeen::start(&get_input("inputs/seventeen")),
+        "eighteen" => eighteen::start(&get_input("inputs/eighteen")),
+        "nineteen" => nineteen::start(&get_input("inputs/nineteen")),
+        "twenty" => twenty::start(&get_input("inputs/twenty")),
+        "twentyone" => twentyone::start(&get_input("inputs/twentyone")),
+        "twentytwo" => twentytwo::start(&get_input("inputs/twentytwo")),
+        "twentythree" => twentythree::start(&get_input("inputs/twentythree")),
+        "twentyfour" => twentyfour::start(&get_input("inputs/twentyfour")),
+        "twentyfive" => twentyfive::start(&get_input("inputs/twentyfive")),
         _ => println!("No matching function"),
     }
 }
