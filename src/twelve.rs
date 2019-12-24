41ecb972a96c3aaa8342ff3c2d559c306c8cf4b9
Micah Smith <micahadamsmith@gmail.com>
2019-12-24 11:58:55 -0500

    12.1 complete

--- src/twelve.rs
+++ src/twelve.rs
@@ -1,48 +1,112 @@
+use std::cmp::Ordering;
 use std::collections::HashSet;
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 struct Moon {
-    position: (i32, i32, i32),
-    velocity: (i32, i32, i32),
+    position: [i32; 3],
+    velocity: [i32; 3],
 }
 
 impl Moon {
-    pub fn new(x: i32, y: i32, z: i32) -> Moon {
+    pub fn new(position: [i32; 3], velocity: Option<[i32; 3]>) -> Moon {
         return Moon {
-            position: (x, y, z),
-            velocity: (0, 0, 0),
+            position: position,
+            velocity: velocity.unwrap_or([0, 0, 0]),
         };
+    }
+
+    pub fn apply_gravity(&mut self, other: &Self) {
+        for i in 0..3 {
+            match self.position[i].cmp(&other.position[i]) {
+                Ordering::Greater => self.velocity[i] -= 1,
+                Ordering::Less => self.velocity[i] += 1,
+                Ordering::Equal => (),
+            }
+        }
+    }
+
+    pub fn apply_velocity(&mut self) {
+        for i in 0..3 {
+            self.position[i] += self.velocity[i];
+        }
+    }
+
+    pub fn get_potential_energy(&self) -> i32 {
+        let mut sum = 0;
+        for i in 0..3 {
+            sum += self.position[i].abs();
+        }
+        return sum;
+    }
+
+    pub fn get_kinetic_energy(&self) -> i32 {
+        let mut sum = 0;
+        for i in 0..3 {
+            sum += self.velocity[i].abs();
+        }
+        return sum;
     }
 }
 
 pub fn start() {
-    let moons = get_moons();
-    simulate(moons, 10);
-
-    println!("{:?}", moons);
+    let mut moons = get_moons();
+    let end = simulate(&mut moons, 1000);
+    let te = find_total_energy(&end);
+    println!("Total energy: {}", te);
 }
 
 fn get_moons() -> HashSet<Moon> {
     let mut moons = HashSet::new();
-    moons.insert(Moon::new(3, 2, -6));
-    moons.insert(Moon::new(-13, 18, 10));
-    moons.insert(Moon::new(-8, -1, 13));
-    moons.insert(Moon::new(5, 10, 4));
+    moons.insert(Moon::new([3, 2, -6], None));
+    moons.insert(Moon::new([-13, 18, 10], None));
+    moons.insert(Moon::new([-8, -1, 13], None));
+    moons.insert(Moon::new([5, 10, 4], None));
     return moons;
 }
 
-fn simulate(mut moons: HashSet<Moon>, steps: i8) {
+fn simulate(moons: &HashSet<Moon>, steps: u16) -> HashSet<Moon> {
+    let mut current = moons.clone();
     for _ in 0..steps {
-        apply_gravity(&mut moons);
-        apply_velocity(&mut moons);
+        current = apply_gravity(&current);
+        current = apply_velocity(&current);
     }
+    return current;
 }
 
-fn apply_gravity(moons: &mut HashSet<Moon>) {
-    for moon in moons.iter() {}
+fn apply_gravity(moons: &HashSet<Moon>) -> HashSet<Moon> {
+    let mut next = HashSet::new();
+    for first in moons.iter() {
+        let mut moon = first.clone();
+        for second in moons.iter() {
+            if first == second {
+                continue;
+            } else {
+                moon.apply_gravity(second);
+            }
+        }
+        next.insert(moon);
+    }
+    return next;
 }
 
-fn apply_velocity(moons: &mut HashSet<Moon>) {}
+fn apply_velocity(moons: &HashSet<Moon>) -> HashSet<Moon> {
+    let mut next = HashSet::new();
+    for first in moons.iter() {
+        let mut moon = first.clone();
+        moon.apply_velocity();
+        next.insert(moon);
+    }
+    return next;
+}
+
+fn find_total_energy(moons: &HashSet<Moon>) -> i32 {
+    let mut te = 0;
+    for moon in moons.iter() {
+        te += moon.get_potential_energy() * moon.get_kinetic_energy();
+    }
+    return te;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,9 +114,12 @@ mod tests {
     #[test]
     fn test_one() {
         let mut moons = HashSet::new();
-        moons.insert(Moon::new(-1, 0, 2));
-        moons.insert(Moon::new(2, -10, -7));
-        moons.insert(Moon::new(4, -8, 8));
-        moons.insert(Moon::new(3, 5, -1));
+        moons.insert(Moon::new([-1, 0, 2], None));
+        moons.insert(Moon::new([2, -10, -7], None));
+        moons.insert(Moon::new([4, -8, 8], None));
+        moons.insert(Moon::new([3, 5, -1], None));
+
+        let end = simulate(&moons, 1);
+        println!("{:?}", end);
     }
 }
