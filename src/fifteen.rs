c4278a5f729152574ad2c9f051464064fe91c2a8
Micah Smith <micahadamsmith@gmail.com>
2019-12-27 12:13:08 -0500

    mapping complete

--- src/fifteen.rs
+++ src/fifteen.rs
@@ -1,7 +1,4 @@
-extern crate rand;
-
 use crate::intcode::IntCodeProgram;
-use rand::Rng;
 use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
@@ -12,49 +9,65 @@ enum DroidMovement {
     East,
 }
 
-fn direction_from_movement(movement: &DroidMovement) -> [i64; 2] {
-    match movement {
-        DroidMovement::North => [0, 1],
-        DroidMovement::South => [0, -1],
-        DroidMovement::West => [-1, 0],
-        DroidMovement::East => [1, 0],
+impl DroidMovement {
+    fn get_direction(&self) -> [i64; 2] {
+        match self {
+            DroidMovement::North => [0, 1],
+            DroidMovement::South => [0, -1],
+            DroidMovement::West => [-1, 0],
+            DroidMovement::East => [1, 0],
+        }
     }
-}
 
-fn instr_from_movement(movement: &DroidMovement) -> i64 {
-    match movement {
-        DroidMovement::North => 1,
-        DroidMovement::South => 2,
-        DroidMovement::West => 3,
-        DroidMovement::East => 4,
+    fn from_direction(dir: [i64; 2]) -> Self {
+        match dir {
+            [0, 1] => DroidMovement::North,
+            [0, -1] => DroidMovement::South,
+            [-1, 0] => DroidMovement::West,
+            [1, 0] => DroidMovement::East,
+            _ => panic!("Unknown direction: {:?}", dir),
+        }
     }
-}
 
-fn change_movement() -> DroidMovement {
-    let mut rng = rand::thread_rng();
-    match rng.gen_range(0, 4) {
-        0 => DroidMovement::North,
-        1 => DroidMovement::South,
-        2 => DroidMovement::West,
-        3 => DroidMovement::East,
-        _ => panic!("Rng out of range"),
+    fn get_instr(&self) -> i64 {
+        match self {
+            DroidMovement::North => 1,
+            DroidMovement::South => 2,
+            DroidMovement::West => 3,
+            DroidMovement::East => 4,
+        }
+    }
+
+    fn rev(&self) -> Self {
+        match self {
+            DroidMovement::North => DroidMovement::South,
+            DroidMovement::South => DroidMovement::North,
+            DroidMovement::West => DroidMovement::East,
+            DroidMovement::East => DroidMovement::West,
+        }
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum DroidStatus {
     Wall,
-    Step,
+    Blank,
     OxSys,
 }
 
-fn status_from_status_code(status_code: i64) -> DroidStatus {
-    match status_code {
-        0 => DroidStatus::Wall,
-        1 => DroidStatus::Step,
-        2 => DroidStatus::OxSys,
-        _ => panic!("Unknown status code: {}", status_code),
+impl DroidStatus {
+    fn from_status_code(status_code: i64) -> Self {
+        match status_code {
+            0 => DroidStatus::Wall,
+            1 => DroidStatus::Blank,
+            2 => DroidStatus::OxSys,
+            _ => panic!("Unknown status code: {}", status_code),
+        }
     }
+}
+
+fn add_points(one: &Point, two: &Point) -> Point {
+    return [one[0] + two[0], one[1] + two[1]];
 }
 
 type Point = [i64; 2];
@@ -63,54 +76,69 @@ type Map = HashMap<Point, DroidStatus>;
 pub fn start(input: &str) {
     let mut program = IntCodeProgram::from_input(&input);
     let map = droid_loop(&mut program);
-    let mh_dist = find_mh_distance(&map);
-    println!("Fewest movements: {}", mh_dist);
+    println!("Fewest movements: {:?}", map);
 }
 
 fn droid_loop(program: &mut IntCodeProgram) -> Map {
+    let mut breadcrumbs = Vec::new();
     let mut map = HashMap::new();
     let mut movement = DroidMovement::North;
     let mut pos = [0, 0];
     let mut status: DroidStatus;
-    map.insert(pos, DroidStatus::Step);
+
+    map.insert(pos, DroidStatus::Blank);
 
     loop {
-        program.in_buf.push(instr_from_movement(&movement));
+        program.in_buf.push(movement.get_instr());
         program.run();
-        status = status_from_status_code(program.out_buf.remove(0));
-        let direction = direction_from_movement(&movement);
+        status = DroidStatus::from_status_code(program.out_buf.remove(0));
+        let direction = movement.get_direction();
 
         match status {
             DroidStatus::Wall => {
-                let wall_pos = [pos[0] + direction[0], pos[1] + direction[1]];
-                map.insert(wall_pos, DroidStatus::Wall);
-                movement = change_movement();
+                map.insert(add_points(&pos, &direction), DroidStatus::Wall);
             }
-            DroidStatus::Step => {
-                pos = [pos[0] + direction[0], pos[1] + direction[1]];
-                map.insert(pos, DroidStatus::Step);
-                movement = change_movement();
+            DroidStatus::Blank => {
+                map.insert(add_points(&pos, &direction), DroidStatus::Blank);
+                pos = add_points(&pos, &direction);
             }
             DroidStatus::OxSys => {
-                pos = [pos[0] + direction[0], pos[1] + direction[1]];
-                map.insert(pos, DroidStatus::OxSys);
-                break;
+                map.insert(add_points(&pos, &direction), DroidStatus::OxSys);
+                pos = add_points(&pos, &direction);
             }
+        }
+
+        if let Some(dir) = find_unexplored(&map, &pos) {
+            movement = dir;
+            breadcrumbs.push(movement.rev());
+        } else if breadcrumbs.len() > 0 {
+            movement = breadcrumbs.pop().unwrap();
+        } else {
+            break;
         }
     }
 
     return map;
 }
 
-fn find_mh_distance(map: &Map) -> u32 {
-    let mut ox_sys = [0, 0];
+fn find_unexplored(map: &Map, pos: &Point) -> Option<DroidMovement> {
+    let directions = [
+        DroidMovement::North,
+        DroidMovement::South,
+        DroidMovement::West,
+        DroidMovement::East,
+    ];
+    let mut unexplored = Vec::new();
 
-    for (pos, status) in map.iter() {
-        if *status == DroidStatus::OxSys {
-            ox_sys = *pos;
-            break;
+    for dir in directions.iter() {
+        let point = add_points(pos, &dir.get_direction());
+        if let None = map.get(&point) {
+            unexplored.push(dir.get_direction());
         }
     }
 
-    return (ox_sys[0].abs() + ox_sys[1].abs()) as u32;
+    if let Some(p) = unexplored.get(0) {
+        return Some(DroidMovement::from_direction(*p));
+    }
+    return None;
 }
