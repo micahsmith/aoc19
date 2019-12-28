fbcde63ad6b6c3af3e08496ce3143f9b8d9c71c6
Micah Smith <micahadamsmith@gmail.com>
2019-12-28 15:50:19 -0500

    16.1 parsed

--- src/sixteen.rs
+++ src/sixteen.rs
@@ -1,3 +1,12 @@
 pub fn start(input: &str) {
-    println!("{}", input);
+    let parsed = parse(input);
+    println!("{:?}", parsed);
+}
+
+fn parse(input: &str) -> Vec<i32> {
+    return input
+        .trim()
+        .chars()
+        .map(|a| a.to_digit(10).unwrap() as i32)
+        .collect();
 }
