ca76b60befe998a4c96f5af6729c819147349c0a
Micah Smith <micahadamsmith@gmail.com>
2019-12-28 16:01:40 -0500

    16.1 in progress

--- src/sixteen.rs
+++ src/sixteen.rs
@@ -1,6 +1,7 @@
 pub fn start(input: &str) {
     let parsed = parse(input);
-    println!("{:?}", parsed);
+    let result = run_fft(&parsed, 100);
+    println!("First eight digits: {}", result);
 }
 
 fn parse(input: &str) -> Vec<i32> {
@@ -9,4 +10,26 @@ fn parse(input: &str) -> Vec<i32> {
         .chars()
         .map(|a| a.to_digit(10).unwrap() as i32)
         .collect();
+}
+
+fn run_fft(input: &Vec<i32>, iters: usize) -> String {
+    let mut current = Vec::new();
+
+    for _ in 0..iters {}
+
+    return String::new();
+}
+
+fn build_pattern(pos: usize) -> Vec<i32> {
+    let base_pattern = vec![0, 1, 0, -1];
+    let mut pattern = Vec::new();
+
+    for base in base_pattern.iter() {
+        for _ in 0..(pos + 1) {
+            pattern.push(*base);
+        }
+    }
+
+    pattern.remove(0);
+    return pattern;
 }
